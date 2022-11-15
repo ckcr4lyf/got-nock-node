9b142ecaa0cd4d3d5c8a517e9cc5ff5afd1d305c
Raghu Saxena <poiasdpoiasd@live.com>
2022-11-15 18:07:23 +0800

    lowercase

--- __tests__/headers.test.js
+++ __tests__/headers.test.js
@@ -4,11 +4,11 @@ import { doStuff } from '../index.js';
 
 test('got gets headers', async t => {
     const nockScope = nock(`https://fake.com`);
-    nockScope.post('/path', { 'key': 'value' }).reply(200, { 'responseKey': 'responseValue' }, { 'X-Custom-Header': '1234' });
+    nockScope.post('/path', { 'key': 'value' }).reply(200, { 'responseKey': 'responseValue' }, { 'x-custom-header': '1234' });
 
     const result = await doStuff();
 
     console.log(result.rawHeaders, result.headers); // just to ilustrate
 
-    t.is(result.headers['X-Custom-Header'], '1234', 'Did not get custom header!');
+    t.is(result.headers['x-custom-header'], '1234', 'Did not get custom header!');
 })
