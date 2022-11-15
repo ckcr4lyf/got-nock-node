1cfec5fd775b9d2762d958527652f648de04f217
Raghu Saxena <poiasdpoiasd@live.com>
2022-11-15 18:26:01 +0800

    axios as well

--- __tests__/headers.test.js
+++ __tests__/headers.test.js
@@ -1,6 +1,6 @@
 import test from 'ava';
 import nock from 'nock';
-import { doStuff } from '../index.js';
+import { doStuff, doStuffWithAxios } from '../index.js';
 
 test('got gets headers', async t => {
     const nockScope = nock(`https://fake.com`);
@@ -8,7 +8,18 @@ test('got gets headers', async t => {
 
     const result = await doStuff();
 
-    console.log(result.rawHeaders, result.headers); // just to ilustrate
+    console.log('GOT CONSOLE LOG', result.rawHeaders, result.headers); // just to ilustrate
 
-    t.is(result.headers['x-custom-header'], '1234', 'Did not get custom header!');
+    t.is(result.headers['x-custom-header'], '1234', 'Did not get custom header from got!');
+})
+
+test('axios gets headers', async t => {
+    const nockScope = nock(`https://fake.com`);
+    nockScope.post('/path', { 'key': 'value' }).reply(200, { 'responseKey': 'responseValue' }, { 'x-custom-header': '1234' });
+
+    const result = await doStuffWithAxios();
+
+    console.log('AXIOS CONSOLE LOG', result.headers); // just to ilustrate
+
+    t.is(result.headers['x-custom-header'], '1234', 'Did not get custom header from axios!');
 })
--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import got from 'got';
+import axios from 'axios';
 
 
 export const doStuff = () => {
     return got.post('https://fake.com/path', { json: { 'key': 'value' }});
 }
 
+export const doStuffWithAxios = () => {
+    return axios.post('https://fake.com/path', { 'key': 'value' });
+}
